2879999974687023aa93bfefd8f3aeabef66bb4c
Alexander Belikov <abelikov@gmail.com>
2017-02-13 09:22:08 -0700

    fixes

--- wos_parser/aux.py
+++ wos_parser/aux.py
@@ -23,23 +23,24 @@ def gunzip_file(fname_in, fname_out):
 
 def main(sourcepath, destpath, global_year):
 
-    only_gz_files = [f for f in listdir(sourcepath) if isfile(join(sourcepath, f)) and f[-2:] == 'gz']
+    only_gz_files = [f for f in listdir(sourcepath) if isfile(join(sourcepath, f)) and f[-3:] == '.gz']
+    good = []
+    bad = []
 
     for f in only_gz_files:
-        full_f = sourcepath + f
-        f_degz = sourcepath + f[:-3]
+        full_f = join(sourcepath, f)
+        f_degz = join(sourcepath, f[:-3])
         gunzip_file(full_f, f_degz)
         xml_remove_trivial_namespace(f_degz)
         with open(f_degz, 'rb') as f:
             good, bad = parse_wos_xml(f, global_year, good, bad)
         logging.info('{0} parsed, {1} good records, {2} bad records'.format(f, len(good), len(bad)))
 
-    if good:
-        fp = gzip.open('{0}good_{1}.pgz'.format(destpath, global_year), 'wb')
-        pickle.dump(good, fp)
-        fp.close()
-    if bad:
-        fp = gzip.open('{0}bad_{1}.pgz'.format(destpath, global_year), 'wb')
-        pickle.dump(bad, fp)
-        fp.close()
+    fp = gzip.open('{0}good_{1}.pgz'.format(destpath, global_year), 'wb')
+    pickle.dump(good, fp)
+    fp.close()
+
+    fp = gzip.open('{0}bad_{1}.pgz'.format(destpath, global_year), 'wb')
+    pickle.dump(bad, fp)
+    fp.close()
 
